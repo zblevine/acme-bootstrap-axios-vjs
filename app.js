e0d0e6d77f3dc3f85a750fd1dc447fd0c5dbeb55
Zach Levine <zblevine@gmail.com>
2019-07-30 21:48:40 -0400

    some little pieces

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const apiURL = 'https://acme-users-api-rev.herokuapp.com/api/'
-const getTD = (str) => `<td>${str}</td>`;
-const getTR = (row) => `<tr>${row}</tr>`;
+const getTags = (type, str) => `<${type}>${str}</${type}>`;
+const compHeaders = ['Id', 'Name', 'Phone', 'State', 'CatchPhrase', 'CreatedAt', 'UpdatedAt'];
+const prodHeaders = ['Id', 'Name', 'Description', 'SuggestedPrice', 'CreatedAt', 'UpdatedAt'];
+const compHeaderRow = compHeaders.map(header => getTags('th', header)).join('');
+const prodHeaderRow = prodHeaders.map(header => getTags('th', header)).join('');
 
 const getCompanyContents = (arr) => arr.map(company => {
     const vals = [company.id, company.name, company.phone, company.state, company.catchPhrase, company.createdAt, company.updatedAt];
-    return getTR(vals.map(attr => getTD(attr)).join(''));
+    return getTags('tr', vals.map(attr => getTags('td', attr)).join(''));
 }).join('');
 
 const getProductsContents = (arr) => arr.map(product => {
-    const vals = [product.id, product.name, product.description, product.suggestPrice, product.createdAt, product.updatedAt];
-    return getTR(vals.map(attr => getTD(attr)).join(''));
+    const vals = [product.id, product.name, product.description, product.suggestedPrice, product.createdAt, product.updatedAt];
+    return getTags('tr', vals.map(attr => getTags('td', attr)).join(''));
 }).join('');
 
 const getData = (tabState) => {
@@ -19,23 +22,24 @@ const getData = (tabState) => {
         const companies = data[1];
         console.log(products);
         console.log(companies);
-        console.log(getProductsContents(products.data))
-        if(tabState === "companies"){
-            document.querySelector('#output').innerHTML = `<table>${getCompanyContents(companies.data)}</table>`;
-        } else if( tabState === "products") {
-            document.querySelector('#output').innerHTML = `<table>${getProductsContents(products.data)}</table>`;    
+        if (tabState === 'companies'){
+            //document.querySelector('#output').innerHTML = `<table>${getCompanyContents(companies.data)}</table>`;
+            document.querySelector('#output').innerHTML = getTags('table', `${getTags('thead', compHeaderRow)}${getTags('tbody', getCompanyContents(companies.data))}`);
+        } else if ( tabState === 'products') {
+            document.querySelector('#output').innerHTML = getTags('table', `${getTags('thead', prodHeaderRow)}${getTags('tbody', getProductsContents(products.data))}`);
         }
+        document.querySelector('table').classList.add('table-striped')
 
     })
     .catch((ex) => console.log(ex.message));
 }
 
-
-
-getData(tabState);
-
 window.addEventListener('hashchange', ()=> {
     let currentSearch = location.hash.split("#")[1];
-}
+    document.querySelector('h3').innerHTML = currentSearch;
+    document.querySelector('#companies-link').classList.toggle('active');
+    document.querySelector('#products-link').classList.toggle('active');
+    getData(currentSearch);
+});
 
-)
+getData('companies');
