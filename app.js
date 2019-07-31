17ea38fbc82b77ede31d8b06c11190bac1b7ce35
Sameehan Patel <sp4315@nyu.edu>
2019-07-30 21:06:06 -0400

    sam's edits

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const apiURL = 'https://acme-users-api-rev.herokuapp.com/api/'
 const getTD = (str) => `<td>${str}</td>`;
 const getTR = (row) => `<tr>${row}</tr>`;
-const getTableContents = (arr) => arr.map(company => {
+
+const getCompanyContents = (arr) => arr.map(company => {
     const vals = [company.id, company.name, company.phone, company.state, company.catchPhrase, company.createdAt, company.updatedAt];
     return getTR(vals.map(attr => getTD(attr)).join(''));
 }).join('');
 
-const getData = () => {
+const getProductsContents = (arr) => arr.map(product => {
+    const vals = [product.id, product.name, product.description, product.suggestPrice, product.createdAt, product.updatedAt];
+    return getTR(vals.map(attr => getTD(attr)).join(''));
+}).join('');
+
+const getData = (tabState) => {
     Promise.all([axios.get(`${apiURL}products`), axios.get(`${apiURL}companies`)])
     .then(data => {
         const products = data[0];
         const companies = data[1];
         console.log(products);
         console.log(companies);
-        document.querySelector('#output').innerHTML = `<table>${getTableContents(companies.data)}</table>`;
+        console.log(getProductsContents(products.data))
+        if(tabState === "companies"){
+            document.querySelector('#output').innerHTML = `<table>${getCompanyContents(companies.data)}</table>`;
+        } else if( tabState === "products") {
+            document.querySelector('#output').innerHTML = `<table>${getProductsContents(products.data)}</table>`;    
+        }
+
     })
     .catch((ex) => console.log(ex.message));
 }
 
-getData();
+
+
+getData(tabState);
+
+window.addEventListener('hashchange', ()=> {
+    let currentSearch = location.hash.split("#")[1];
+}
+
+)
